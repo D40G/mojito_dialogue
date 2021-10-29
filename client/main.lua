ff7a871871fc0f87d43697f38077e34f32031432
Liam Dormon <66041893+Mojito-Fivem@users.noreply.github.com>
2021-10-29 16:08:50 +0100

    chore: Remove debug rendering

--- client/main.lua
+++ client/main.lua
@@ -16,7 +16,7 @@ local function NewDialogue(ped, coords, radius, options, callback)
     local index = #Peds + 1
     local zone = CircleZone:Create(coords, radius, {
         name = prefix .. index,
-        debugPoly = true
+        debugPoly = false
     })
     Peds[index] = {
         zone = zone,
@@ -41,7 +41,7 @@ local function NewDialogueSync(ped, coords, radius, options)
     local index = #Peds + 1
     local zone = CircleZone:Create(coords, radius, {
         name = prefix .. index,
-        debugPoly = true
+        debugPoly = false
     })
     Peds[index] = {
         zone = zone,
